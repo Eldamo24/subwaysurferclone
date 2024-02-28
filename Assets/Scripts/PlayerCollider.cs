f4f96b51bec6032ce4974db9167eda68c7a192f5
Damian Lobos <damianlobos24@gmail.com>
2024-02-28 13:39:02 -0300

    Collision system

---
+++ Assets/Scripts/PlayerCollider.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollider : MonoBehaviour
+{
+    [SerializeField] private PlayerCollision playerCollision;
+
+
+    void Awake()
+    {
+        playerCollision = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerCollision>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            return;
+        playerCollision.OnCharacterCollision(collision.collider);
+    }
+}
